d07f6df6a59607bfb971950e02a3ba4ecea7cf6d
Atolmspek <martin.docenciacip@gmail.com>
2023-11-28 14:09:24 +0100

    Final touches to Coin component, started calc

--- src/main/components/Conversor/Board.tsx
+++ src/main/components/Conversor/Board.tsx
@@ -1,8 +1,10 @@
+import { Calculator } from "./Calculator";
 import { CoinConversor } from "./CoinConversor";
 
 export const Board = () => {
 
     return (
-    <CoinConversor/>
+   // <CoinConversor/>
+   <Calculator/>
     );
 }
